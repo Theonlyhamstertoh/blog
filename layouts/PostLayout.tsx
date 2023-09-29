fc2b51515fd07b64d5f493550bab2e7dc57a37f4
weeeeebo <75579372+Theonlyhamstertoh@users.noreply.github.com>
2023-09-29 00:03:21 -0700

    wrote article on intentional-vs-passive education, added newsletter to postlayout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -9,6 +9,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
@@ -111,6 +112,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+              <div className="py-8  sm:px-4 sm:mx-auto flex items-center justify-center  dark:border-t-gray-700">
+                <NewsletterForm />
+              </div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(path)} rel="nofollow">
                   Discuss on Twitter
